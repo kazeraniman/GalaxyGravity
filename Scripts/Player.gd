7b3f75564400d5622672b962c6c978cbc36b3223
Iman Kazerani <kazeraniman@gmail.com>
2023-11-19 00:33:07 -0500

    Adding basic functionality for gravity on a sphere. Logic should form the basis for all forms of gravity moving forward since it is no longer just pointing down.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,19 +1,26 @@
 extends CharacterBody3D
 
-@export var speed = 5
-@export var jump_impulse = 5
-@export var gravity = 9.8
+@export var speed : float = 5
+@export var jump_impulse : float = 5
+@export var gravity : float = 9.8
 
+var gravity_vector : Vector3 = Vector3.DOWN
 
 func _physics_process(delta):
 	# Check directional inputs
-	var forwardStrength = Input.get_action_strength("move_forward")
-	var backwardStrength = Input.get_action_strength("move_backward")
-	var leftStrength = Input.get_action_strength("move_left")
-	var rightStrength = Input.get_action_strength("move_right")
-	
+	var forwardStrength : float = Input.get_action_strength("move_forward")
+	var backwardStrength : float = Input.get_action_strength("move_backward")
+	var leftStrength : float = Input.get_action_strength("move_left")
+	var rightStrength : float = Input.get_action_strength("move_right")
+
+	# Determine gravity and normal
+	var old_gravity_vector : Vector3 = gravity_vector
+	gravity_vector = (Vector3.ZERO - position).normalized()
+	var normal_vector : Vector3 = -gravity_vector
+	up_direction = normal_vector
+
 	# Determine the "ground" movement direction
-	var direction = Vector3.ZERO
+	var direction : Vector3 = Vector3.ZERO
 	if forwardStrength > 0:
 		direction.z -= forwardStrength
 	if backwardStrength > 0:
@@ -22,24 +29,35 @@ func _physics_process(delta):
 		direction.x -= leftStrength
 	if rightStrength > 0:
 		direction.x += rightStrength
-	
+
 	# Normalize the movement to avoid super-speed diagonals
 	if direction.length() > 1:
 		direction = direction.normalized()
-	
+
+	# Rotate the player to face the direction of movement
+	if direction != Vector3.ZERO:
+		$ModelContainer.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP)
+
+	# Rotate with respect to gravity
+	var rotation_axis : Vector3 = (Vector3.UP.cross(normal_vector)).normalized()
+	var rotation_angle : float = Vector3.UP.angle_to(normal_vector)
+	if (rotation_axis != Vector3.ZERO):
+		direction = direction.rotated(rotation_axis, rotation_angle).normalized()
+		transform.basis = Basis(rotation_axis, rotation_angle)
+
 	# Determine the scaled, ground velocity
-	var new_velocity = direction * speed
-	
+	var new_velocity : Vector3 = direction * speed
+
 	# Retain gravity's influence
-	new_velocity.y = velocity.y
-	
+	new_velocity += velocity.project(old_gravity_vector)
+
 	# Jumping
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		new_velocity.y = jump_impulse
-	
+		new_velocity += jump_impulse * normal_vector
+
 	# Gravity
-	new_velocity.y -= gravity * delta
-	
+	new_velocity += gravity * delta * gravity_vector
+
 	# Move and perform collisions
 	velocity = new_velocity
 	move_and_slide()
