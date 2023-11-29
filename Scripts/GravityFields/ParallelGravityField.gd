75061be5a9e32c7b7fef62dc77176cb81545cf0f
Iman Kazerani <kazeraniman@gmail.com>
2023-11-29 02:36:30 -0500

    Switching to using shaders rather than particle systems as that was the intention in the first place.

--- Scripts/GravityFields/ParallelGravityField.gd
+++ Scripts/GravityFields/ParallelGravityField.gd
@@ -2,9 +2,14 @@
 extends GravityField
 
 @onready var collision_shape_3d: CollisionShape3D = $Collider/CollisionShape3D
+@onready var debug_visuals: MeshInstance3D = $DebugVisuals
 
 @export var size: Vector3 = Vector3.ONE: set = set_size
 
+func _ready():
+	super._ready()
+
+	debug_visuals.visible = Engine.is_editor_hint()
 
 func get_gravity(_target_position: Vector3) -> Vector3:
 	return gravity * (-global_transform.basis.y).normalized()
@@ -17,5 +22,11 @@ func set_size(new_size: Vector3):
 
 	if Engine.is_editor_hint() and collision_shape_3d == null:
 		collision_shape_3d = $Collider/CollisionShape3D
+		debug_visuals = $DebugVisuals
 
 	collision_shape_3d.shape.size = new_size
+	debug_visuals.mesh.size = new_size
+	debug_visuals.mesh.material.set_shader_parameter("height", new_size.y)
+
+func toggle_debug():
+	debug_visuals.visible = not debug_visuals.visible
--- Scripts/GravityFields/SphericalGravityField.gd
+++ Scripts/GravityFields/SphericalGravityField.gd
@@ -2,7 +2,7 @@
 extends GravityField
 
 @onready var collision_shape_3d: CollisionShape3D = $Collider/CollisionShape3D
-@onready var cpu_particles_3d: CPUParticles3D = $CPUParticles3D
+@onready var debug_visuals: MeshInstance3D = $DebugVisuals
 
 @export var radius: float = 1: set = set_radius
 
@@ -10,7 +10,7 @@ extends GravityField
 func _ready():
 	super._ready()
 
-	cpu_particles_3d.emitting = Engine.is_editor_hint()
+	debug_visuals.visible = Engine.is_editor_hint()
 
 func get_gravity(target_position: Vector3) -> Vector3:
 	return gravity * (global_position - target_position).normalized()
@@ -23,11 +23,11 @@ func set_radius(new_radius: float):
 
 	if Engine.is_editor_hint() and collision_shape_3d == null:
 		collision_shape_3d = $Collider/CollisionShape3D
-		cpu_particles_3d = $CPUParticles3D
+		debug_visuals = $DebugVisuals
 
 	collision_shape_3d.shape.radius = new_radius
-	cpu_particles_3d.mesh.radius = new_radius
-	cpu_particles_3d.mesh.height = new_radius * 2
+	debug_visuals.mesh.radius = new_radius
+	debug_visuals.mesh.height = new_radius * 2
 
 func toggle_debug():
-	cpu_particles_3d.emitting = not cpu_particles_3d.emitting
+	debug_visuals.visible = not debug_visuals.visible
